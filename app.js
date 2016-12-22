d99dddd94540fcd8e9f3fd599bbe242181a3704b
Johnson Lu <JohnsonLu@gmail.com>
2016-12-22 17:44:13 -0500

    Added a grid for the maze to crave out

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-function setup(){
-    createCanvas(640,480);
+var cellSize = 20;
+var height = 500;
+var width = 500;
+var grid = [];
+
+function setup() {
+    createCanvas(500, 500);
     background(51);
+    for (var y = 0; y < floor(height / cellSize); y++) {
+        for (var x = 0; x < floor(width / cellSize); x++) {
+            var cell = new Cell(x, y);
+            grid.push(cell);
+        }
+    }
 }
 
-function draw(){
-   
+function draw() {
+    for (var i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+}
+
+function Cell(x, y) {
+    var i = x * cellSize;
+    var j = y * cellSize;
+    
+    this.show = function () {
+        stroke(255,255,255);
+        // top line
+        line(i, j, i + cellSize, j);
+        // right line
+        line(i + cellSize, j, i + cellSize, j + cellSize);
+        // bottom line
+        line(i, j + cellSize, i + cellSize, j + cellSize);
+        // left line
+        line(i, j, i, j + cellSize);
+    }
 }
