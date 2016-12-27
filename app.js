df67f1cbf250ec4b79b06d22fdd63da864c1834b
Johnson Lu <JohnsonLu@gmail.com>
2016-12-27 12:14:27 -0500

    working as intended

--- app.js
+++ app.js
@@ -7,104 +7,106 @@ var currentCell;
 var checked = [];
 
 function setup() {
-    createCanvas(500, 500);
-    cols = floor(width/cellSize);
-	rows = floor(height/cellSize);
+	createCanvas(500, 500);
+	cols = floor(width / cellSize);
+	rows = floor(height / cellSize);
 
 	background(25);
-    for (var y = 0; y < floor(height / cellSize); y++) {
-        for (var x = 0; x < floor(width / cellSize); x++) {
-            var cell = new Cell(x, y);
-            grid.push(cell);
-        }
-    }
-    currentCell = grid[0];
+	for (var y = 0; y < floor(height / cellSize); y++) {
+		for (var x = 0; x < floor(width / cellSize); x++) {
+			var cell = new Cell(x, y);
+			grid.push(cell);
+		}
+	}
+	currentCell = grid[0];
 }
 
 function draw() {
-    for (var i = 0; i < grid.length; i++) {
-        grid[i].show();
-    }
-    currentCell.visited = true;
+	for (var i = 0; i < grid.length; i++) {
+		grid[i].show();
+	}
+	currentCell.visited = true;
 	currentCell.current = true;
 	var next = currentCell.checkNeighbors();
-	if(next){
+	if (next) {
 		next.visit = true;
 		checked.push(currentCell);
 		removeWalls(currentCell, next);
 		currentCell = next;
-	}else if (checked.length > 0){
+	} else if (checked.length > 0) {
 		currentCell = checked.pop();
 	}
 }
 
-function index(x,y){
-	if(x < 0 || y < 0 || x > cols - 1 || y > rows - 1){
+function index(x, y) {
+	if (x < 0 || y < 0 || x > cols - 1 || y > rows - 1) {
 		return -1;
 	}
 	return x + y * cols;
 }
 
-function removeWalls(a, b){
+function removeWalls(a, b) {
 	var x = a.i - b.i;
-	if(x >= 0){
+	console.log("x: " + x);
+	if (x > 0) {
+		a.Walls[3] = false;
+		b.Walls[1] = false;
+	} else if (x < 0) {
+		a.Walls[1] = false;
+		b.Walls[3] = false;
+	}
+	var y = a.j - b.j;
+	if(y > 0){
 		a.Walls[0] = false;
 		b.Walls[2] = false;
-	}else if(x < 0){
+	}else if(y < 0){
 		a.Walls[2] = false;
 		b.Walls[0] = false;
 	}
-	var y = a.j - b.j;
-	if(y >= 0){
-		a.Walls[1] = false;
-		b.Walls[3] = false;
-	}else if(y < 0){
-		a.Walls[3] = false;
-		b.Walls[1] = false;
-	}
+
 }
 
 function Cell(x, y) {
-    this.i = x * cellSize;
-    this.j = y * cellSize;
-    this.Walls = [true, true, true, true];
-    this.visited = false;
+	this.i = x * cellSize;
+	this.j = y * cellSize;
+	this.Walls = [true, true, true, true];
+	this.visited = false;
 	this.current = false;
-    
-    this.show = function () {
-        stroke(255, 255, 255);
-        if (this.Walls[0] === true) {
-            // top line
-            line(this.i, this.j, this.i + cellSize, this.j);
-        }
-        if (this.Walls[1] === true) {
-            // right line
-            line(this.i + cellSize, this.j, this.i + cellSize, this.j + cellSize);
-        }
-        if (this.Walls[2] === true) {
-            // bottom line
-            line(this.i, this.j + cellSize, this.i + cellSize, this.j + cellSize);
-        }
-        if (this.Walls[3] === true) {
-            // left line
-            line(this.i, this.j, this.i, this.j + cellSize);
-        }
 
-		if(this.current){
+	this.show = function () {
+		stroke(255, 255, 255);
+		if (this.Walls[0] === true) {
+			// top line
+			line(this.i, this.j, this.i + cellSize, this.j);
+		}
+		if (this.Walls[1] === true) {
+			// right line
+			line(this.i + cellSize, this.j, this.i + cellSize, this.j + cellSize);
+		}
+		if (this.Walls[2] === true) {
+			// bottom line
+			line(this.i, this.j + cellSize, this.i + cellSize, this.j + cellSize);
+		}
+		if (this.Walls[3] === true) {
+			// left line
+			line(this.i, this.j, this.i, this.j + cellSize);
+		}
+
+		if (this.current) {
 			noStroke();
 			fill(85, 170, 85, 255);
-			rect(this.i,this.j,cellSize,cellSize);
+			rect(this.i, this.j, cellSize, cellSize);
 		}
 
-        if(this.visited){
-            noStroke();
-            fill(20, 48, 84, 100);
-            rect(this.i, this.j, cellSize, cellSize);
-        	this.current = false;
+		if (this.visited) {
+			noStroke();
+			fill(20, 48, 84, 100);
+			rect(this.i, this.j, cellSize, cellSize);
+			this.current = false;
 		}
-    }
+	}
 
-	this.checkNeighbors = function(){
+	this.checkNeighbors = function () {
 		var Neighbors = [];
 
 		var top = grid[index(x, y - 1)];
@@ -112,21 +114,21 @@ function Cell(x, y) {
 		var bottom = grid[index(x, y + 1)];
 		var left = grid[index(x - 1, y)];
 
-		if(top && !top.visited){
+		if (top && !top.visited) {
 			Neighbors.push(top);
 		}
-		if(right && !right.visited){
+		if (right && !right.visited) {
 			Neighbors.push(right);
 		}
-		if(bottom && !bottom.visited){
+		if (bottom && !bottom.visited) {
 			Neighbors.push(bottom);
 		}
-		if(left && !left.visited){
+		if (left && !left.visited) {
 			Neighbors.push(left);
 		}
-		if(Neighbors.length > 0) {
+		if (Neighbors.length > 0) {
 			return random(Neighbors);
-		}else{
+		} else {
 			return undefined;
 		}
 	}
